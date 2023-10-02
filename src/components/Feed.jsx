308d0af69d6e81a3ad6f9e08a380ef1fae08a10e
Rubie02 <hoainn9a@gmail.com>
2023-10-02 20:33:43 +0700

    edit footer details

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -21,7 +21,7 @@ const Feed = () => {
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 2023 | Rubie
+          Copyright © 2023 | Rubie.
         </Typography>
       </Box>
 
